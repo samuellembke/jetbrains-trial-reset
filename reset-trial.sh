73a83f65e8602b03076965d2cf2318aa1f63db7a
samuellembke <72019722+samuellembke@users.noreply.github.com>
2021-05-03 10:38:10 +0200

    Implemented dynamic product list and added new eval strings

--- reset-trial.sh
+++ reset-trial.sh
@@ -6,15 +6,24 @@
 
 echo 'Resetting...'
 
-possible_products=(DataGrip2020.3 IntelliJIdea2020.3 PhpStorm2020.3 WebStorm2020.3)
+echo 'Deleting jetbrains java userPrefs'
 
-eval_strings=(evlsprt.203 evlsprt2.203 evlsprt3.202 evlsprt3.203)
+rm ~/.java/.userPrefs/jetbrains -r
+
+possible_products=$( ls ~/.config/JetBrains/ )
+
+echo "Products found in ~/.config/JetBrains:"
+
+echo $possible_products;
+
+eval_strings=(evlsprt.203 evlsprt2.203 evlsprt3.202 evlsprt3.203 evlsprt.211 evlsprt2.211 evlsprt3.211)
 
 for i in ${possible_products[@]}; do
         echo 'Removing eval-file for: '$i
         rm -R ~/.config/JetBrains/$i/eval &> /dev/null
         echo 'Removing license relations in other.xml for: ' $i
         for e in ${eval_strings[@]}; do
+                xmlstarlet ed -L -d "/application/component/option[@name='trackers']" ~/.config/JetBrains/$i/options/other.xml
                 xmlstarlet ed -L -d "/application/component/property[@name='"$e"']" ~/.config/JetBrains/$i/options/other.xml
         done
 done
